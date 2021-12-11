650aea1951cbe89a160fcdd11da965247cabbd63
Akshat Mantry <akshatmantry@gmail.com>
2021-12-11 18:06:19 +0530

    added fastPower.java

---
+++ Arithmetic_Problems/fastPower.java
@@ -0,0 +1,15 @@
+public class fastPower {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 3;
+        int result = 1;
+        while (exponent > 0) {
+            if((exponent & 1) == 1) {
+                result *= base;
+            }
+            exponent >>= 1;
+            base *= base;
+        }
+        System.out.println(result);
+    }
+}
